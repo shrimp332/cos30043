a83ac1bdc02e31b7e184636df04b330ee41d3af0
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2025-05-23 22:04:03 +1000

    style home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,23 @@
 <template>
-    <h1>Articulate</h1>
-    <p>Welcome, here you can upload your own articles on topics of your own choosing. Share your opinion, tutorials on your hobbies, recipes. Anything you want to write about can be written here.</p>
-    <img src="/turtle.jpg" width="600">
-    <img src="/giraffe.jpg" width="600">
+    <h1 class="pt-4 text-center">Articulate</h1>
+    <p class="ms-3 me-3 text-center ">Welcome, here you can upload your own articles on topics of your own
+        choosing.
+        Share your opinion, tutorials on your hobbies, recipes. Anything you want to write about can be written
+        here.</p>
+    <div class="container">
+        <div class="row">
+            <div class="ms-0 me-0 p-0 col-12 col-md-6 ">
+                <img class="img-fluid" src="/giraffe.jpg">
+            </div>
+            <div class="ms-0 me-0 p-0 col-12 col-md-6 ">
+                <img class="img-fluid" src="/turtle.jpg">
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+img {
+    filter: brightness(1.5);
+}
 </style>

30fcaeed1602a430b47a572fd0de874384425fc0
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2025-05-23 20:59:58 +1000

    add home content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,9 @@
 <template>
-    <h1>Home</h1>
+    <h1>Articulate</h1>
+    <p>Welcome, here you can upload your own articles on topics of your own choosing. Share your opinion, tutorials on your hobbies, recipes. Anything you want to write about can be written here.</p>
+    <img src="/turtle.jpg" width="600">
+    <img src="/giraffe.jpg" width="600">
 </template>
 
 <style scoped>
 </style>
-

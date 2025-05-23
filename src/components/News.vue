ee9ac4464028e42fa32513619ee635dd25b633d4
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2025-05-23 23:21:19 +1000

    style news

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,25 +1,66 @@
 <template>
-    <h1>News</h1>
-    <input type="text" @input="page = 1" v-model="searchTitle">
-    <input type="text" @input="page = 1" v-model="searchContent">
-    <input type="date" @input="page = 1" v-model="searchDate">
-    <button v-if="searchDate != ''" @click="searchDate = ''; page = 1">Reset Date</button>
-    <select @change="page = 1" v-model="searchCategory">
-        <option value="all" selected>All</option>
-        <option value="blog" selected>Blog</option>
-        <option value="update">Update</option>
-        <option value="pricing">Pricing</option>
-        <option value="disclosure">Disclosure</option>
-        <option value="world">World</option>
-    </select>
-    <div v-for="item in filteredNews.slice((this.page - 1) * 10, ((this.page - 1) * 10) + 10)">
-        {{ item.title }}
-        {{ item.date }}
-        {{ item.category }}
-        {{ item.content }}
+    <h1 class="text-center mt-4 mb-4">News</h1>
+
+    <div class="container row mx-auto">
+        <div class="col-12 col-md-6">
+            <label for="title-search">Title:</label>
+            <input id="title-search" class="form-control" type="text" @input="page = 1" v-model="searchTitle">
+            <label for="cont-search">Contents:</label>
+            <input id="cont-search" class="form-control" type="text" @input="page = 1" v-model="searchContent">
+
+        </div>
+        <div class="col-12 col-md-6">
+
+            <label for="date-search">Date:</label>
+            <div class="input-group">
+                <input id="date-search" class="form-control" type="date" @input="page = 1" v-model="searchDate">
+                <button class="btn btn-warning" v-if="searchDate != ''" @click="searchDate = ''; page = 1">Reset
+                    Date</button>
+            </div>
+
+            <label for="select-search">Category:</label>
+            <select id="select-search" class="form-select" @change="page = 1" v-model="searchCategory">
+                <option value="all" selected>All</option>
+                <option value="blog" selected>Blog</option>
+                <option value="update">Update</option>
+                <option value="pricing">Pricing</option>
+                <option value="disclosure">Disclosure</option>
+                <option value="world">World</option>
+            </select>
+        </div>
     </div>
-    <button v-if="page > 1" @click="page--">Previous Page</button>
-    <button v-if="isMaxPage" @click="page++">Next Page</button>
+
+    <div class="d-flex justify-content-center">
+        <div class="container">
+            <div class="row">
+                <div v-for="item in filteredNews.slice((this.page - 1) * 10, ((this.page - 1) * 10) + 10)"
+                    class="col-12 col-md-6">
+                    <div class="card mt-4">
+                        <div class="card-header d-flex justify-content-between">
+                            <span class="ms-2">{{ item.title }}</span>
+                            <span class="me-2">{{ item.category }}</span>
+                        </div>
+                        <div class="card-body">
+                            {{ item.content }}
+                        </div>
+                        <div class="card-footer p-1">
+                            <span class="ms-2">{{ item.date }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <button class="btn btn-secondary my-3" v-if="page > 1" @click="page--">Previous Page</button>
+                </div>
+                <div class="col-12 col-md-6">
+                    <button class="btn btn-secondary ms-auto my-3" v-if="isMaxPage" @click="page++">Next Page</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </template>
 
 <script>

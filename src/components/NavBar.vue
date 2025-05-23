58cf8e2f800969c4e3d290e2e6aee769e3c0f0ae
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2025-05-23 21:27:52 +1000

    style nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,11 @@
 <template>
-    <router-link to="/">Home</router-link>
-    <router-link to="/app">App</router-link>
-    <router-link to="/news">News</router-link>
-    <router-link to="/about">About</router-link>
+    <nav class="navbar bg-dark text-white">
+        <router-link class="fs-5 text-reset" style="margin-left:1em; margin-right: 1em;" to="/">Home</router-link>
+        <router-link class="fs-5 text-reset" style="margin-right: 1em;" to="/app">App</router-link>
+        <router-link class="fs-5 text-reset" style="margin-right: 1em;" to="/news">News</router-link>
+        <router-link class="fs-5 text-reset" style="margin-left:auto; margin-right: 1em;" to="/login">Login</router-link>
+        <router-link class="fs-5 text-reset" style="margin-right: 1em;" to="/about">About</router-link>
+    </nav>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>

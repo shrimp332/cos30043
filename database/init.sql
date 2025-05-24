d633fe13eb1fa85cb5d589e12d4b32ed43a92656
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2025-05-25 02:04:25 +1000

    update schema
    - posts
    - likes
    - comments

--- database/init.sql
+++ database/init.sql
@@ -12,12 +12,12 @@ CREATE TABLE IF NOT EXISTS UserSessions (
     session_id CHAR(32) PRIMARY KEY NOT NULL,
     user_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
 DELIMITER $$
 
-CREATE EVENT IF NOT EXISTS delete_old_users
+CREATE EVENT IF NOT EXISTS delete_old_sessions
 ON SCHEDULE EVERY 4 HOUR
 DO
 BEGIN
@@ -28,4 +28,31 @@ END $$
 DELIMITER ;
 
 -- password is 'password' (Very secure good job)
-INSERT INTO Users (username, password_hash) VALUES ('shrimp332', '$2y$10$13BTa74Q0.vgdvCvpmlFc.ddEg9Zzbb6YHCKeiXrVRkFDpdmpsy9q')
+-- INSERT INTO Users (username, password_hash) VALUES ('shrimp332', '$2y$10$13BTa74Q0.vgdvCvpmlFc.ddEg9Zzbb6YHCKeiXrVRkFDpdmpsy9q')
+
+CREATE TABLE IF NOT EXISTS Posts (
+    post_id INT AUTO_INCREMENT PRIMARY KEY,
+    title NVARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    owner_id INT NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES Users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Likes (
+    user_id INT,
+    post_id INT
+    PRIMARY KEY (user_id, post_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES Posts(post_id) ON DELETE CASCADE
+);
+ 
+CREATE TABLE IF NOT EXISTS Comments (
+    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    owner_id INT NOT NULL,
+    post_id INT NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES Posts(post_id) ON DELETE CASCADE
+);
